60f1d38ec82945e9b974d6b6debcb62cf80e03d2
Tim Lynch <timothy.lynch61@spsmail.cuny.edu>
2022-08-28 15:15:58 -0400

    Completed Book Class Assignment

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,22 @@
 
 class Book:
     def __init__(self, author, title):
-        pass
-    
+        """
+        Constructor of Book class
+        :param author: The author of the book
+        :param title: The title of the book
+        """
+
+        self.author = author
+        self.title = title
+
     def display(self):
-        pass
+        print(f"{self.title} written by {self.author}")
 
 
 if __name__ == "__main__":
-    pass
+    book1 = Book("John Steinbeck", "Of Mice and Men")
+    book2 = Book("Harper Lee", "To Kill a Mockinbird")
+
+    book1.display()
+    book2.display()
