2661c120bf391ddf4b49d7f3ee98b81905339f33
Tim Lynch <timothy.lynch61@spsmail.cuny.edu>
2022-08-28 16:24:20 -0400

    Completed assignment1_part1

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,36 @@
+class List_Divide_Exception(Exception):
+    def __init__(self, message = "Assertion Error in list_divide"):
+        self.message = message
 
-def list_divide(numbers, divide):
+    def __str__(self):
+        return f'{self.message} - made by Tim'
+
+def list_divide(numbers, divide = 2):
     """
-    The function returns the number of elements in the numbers list that are divisibleby divide
+    The function returns the number of elements in the numbers list that are divisible by divide
+    :param numbers: List(array in JS) of numbers
+    :param divide: Number to divide list[i] by, default = 2
     """
-    pass
+    count = 0
+    for num in numbers:
+        if num % divide == 0:
+            count += 1
+    return count
 
 def test_list_divide():
     """
     Test listDivide
     """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
+    try:
+        assert list_divide([1,2,3,4,5]) == 2
+        assert list_divide([2,4,6,8,10]) == 5
+        assert list_divide([30, 54, 63,98, 100], divide=10) == 2
+        assert list_divide([]) == 0
+        assert list_divide([1,2,3,4,5], 1) == 5
+    except AssertionError as e:
+        raise List_Divide_Exception(e) from None
+    else: 
+        print('No errors: All Assertion Tests pass')
     
 if __name__ == "__main__":
-    testListDivide()
+    test_list_divide()
